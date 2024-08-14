5d2ef789c85bf8d091550ae3b6e45deb1f843abe
Sara Elnaggar <sara.elnaggar.114@gmail.com>
2024-08-14 23:19:39 +0300

    Updating

---
+++ SportsWorld/Modules/Leagues/ViewModel/LeaguesViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  LeaguesViewModel.swift
+//  SportsWorld
+//
+//  Created by Sarah on 14/08/2024.
+//
+
+import Foundation
+
+class LeaguesViewModel {
+    
+    var networkHandler: Networkprotocol?
+    var coreDataManager: CoreDataProtocol?
+    var bindResultToViewController : (()->()) = {}
+    var sport: String!
+    var result : [League]?  {
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    
+    init() {
+        networkHandler = Network()
+        coreDataManager = CoreDataManager()
+    }
+    
+    func loadDataFromApi(){
+        let urlManager = URLManger()
+        networkHandler?.fetch(url: urlManager.getFullURL(sport: sport, detail: "allLeagues") , type: Leagues.self, complitionHandler: { [weak self] leagues in
+            guard let self = self else { return }
+            if let leagues = leagues {
+                self.result = leagues.result
+            } else {
+                print("Failed to fetch or decode leagues")
+            }
+        })
+    }
+
+    
+    func loadDatafromCoreData(){
+        coreDataManager?.fetchFromCoreData()
+        
+    }
+    
+    func getLeagues()->[League]{
+        return result ?? []
+    }
+    
+    
+}
